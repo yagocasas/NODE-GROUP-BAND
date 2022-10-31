2479fc9824c71679df0eac2fb8e1207bdd917733
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-31 04:04:48 +0100

    FIX, lasts changes of the night

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-require('dotenv').config();
 const bandsRoutes = require('./src/api/bands/bands.routes');
 const albumRoutes = require('./src/api/discography/discography.routes');
 const usersRoutes = require('./src/api/users/users.routes');
+const cors = require('cors');
+require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
-
 const db = require('./src/utils/database/db');
 db.connectDb();
 
@@ -18,7 +18,15 @@ const PORT = process.env.PORT || 8080;
 const DB_URL = process.env.DB_URL;
 
 const server = express();
+
+server.use(cors({
+    origin: '*',
+    credentials: true
+}))
+
 const router = express.Router();
+
+
 
 server.use(express.json());
 server.use('/users', usersRoutes)
